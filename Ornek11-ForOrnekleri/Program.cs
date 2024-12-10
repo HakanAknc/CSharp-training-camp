de92029a139d6150d22d478bb58641573e412b34
Hakan Akıncı <96823170+HakanAknc@users.noreply.github.com>
2024-12-10 10:11:23 +0300

    Add files via upload

---
+++ Ornek11-ForOrnekleri/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornek11_ForOrnekleri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ÖRNEK: ( Sayıları ritmik arttırma )
+
+            //for (int adim = 2; adim<=100; adim=adim+2)
+            //{
+            //    Console.WriteLine(adim);
+            //}
+
+
+            // ÖRNEK: (Girilen sayılardan en büyük/küçük olanını bulma)
+            //int buyuk = 0, kucuk = 0, sayi = 0;
+            //for (int i = 1; i<= 5; i++)
+            //{
+            //    Console.Write((i) + ".sayıyı giriniz: ");
+            //    sayi = Convert.ToInt32(Console.ReadLine());
+
+            //    if (i == 1)
+            //    {
+            //        buyuk = sayi;
+            //        kucuk = sayi;
+            //    }
+            //    if (kucuk>sayi)
+            //    {
+            //        kucuk = sayi;
+            //    }
+            //    if (buyuk < sayi)
+            //    {
+            //        buyuk = sayi;
+            //    }
+            //}
+            //Console.WriteLine("En büyük sayi: " + buyuk);
+            //Console.WriteLine("Enküçük sayi: " + kucuk);
+
+
+            // ÖRNEK: ( Sayının katını bulma )
+            //int sayi;
+            //Console.Write("Sayı giriniz: ");
+            //sayi = Convert.ToInt32(Console.ReadLine());
+            //if (sayi % 7 == 0)
+            //{
+            //    Console.WriteLine("Sayı yedinin katıdır.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Sayı yedinin katı değildir.");
+            //}
+
+
+            //ÖRNEK: ( Sayıların ortalamasını bulma )
+            //int s = 0;
+            //for (int i = 1; i<=5; i++)
+            //{
+            //    Console.Write(i + ". sayıyı giriniz: ");
+            //    int sayi = Convert.ToInt32(Console.ReadLine());
+            //    s += sayi;  // s = s + sayi  s = 11 + 12 = 23
+            //}
+            //Console.WriteLine("Toplam : " + s);
+            //Console.WriteLine("Ortalama : " + s / 5);
+
+
+            //ÖRNEK: ( Ekrana otomatik ilerleyen yazı yazdırma )
+            for (int a = 0; a < 100; a++)
+            {
+                Console.SetCursorPosition(a, 15);
+                Console.Write(" Risale-i Nur  ==  Bediüzzaman Said Nursi (ra) ");
+                System.Threading.Thread.Sleep(100);
+            }
+
+
+
+            Console.Read();
+        }
+    }
+}
---
+++ Ornek12_ForOrnekleri2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornek12_ForOrnekleri2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ÖRNEK: ( Random ile sayıların toplamını bulma )
+            //Random sayi;
+            //sayi = new Random();
+            //int toplam = 0;
+            //int bts = sayi.Next(0,100);
+            //for (int a = 1; a <= 10; a++)
+            //{
+            //    bts = sayi.Next(0, 100);
+            //    Console.WriteLine(bts);
+            //    toplam = toplam + bts;
+            //}
+            //Console.WriteLine("Toplam : " + toplam);
+
+
+            // ÖRNEK: ( Sayı tahmini yapma )
+            //Random sayiUret;
+            //sayiUret = new Random();
+            //int bts = sayiUret.Next(1, 100);
+            //Console.Write("Tahmininizi giriniz:");
+            //int tahmin = Convert.ToInt32(Console.ReadLine());
+            //if (tahmin == bts)
+            //{
+            //    Console.WriteLine("DOĞRU TAHMİN");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("MAALESEF OLMADI");
+            //}
+
+
+            // ÖRNEK: ( Kullanıcının yaşını ve gününü bulma )
+            DateTime zaman;
+            zaman = new DateTime();
+            zaman = DateTime.Now;
+            Console.WriteLine("Bu günün tarihi:" + zaman.ToString());
+            Console.Write("Doğum tarihinizi giriniz:");
+            DateTime dogumtarihi;
+            dogumtarihi = Convert.ToDateTime(Console.ReadLine());
+            TimeSpan gün = zaman - dogumtarihi;
+            double toplamgün = gün.Days;
+            Console.WriteLine(toplamgün + " Günlüksünüz");
+            int yas = zaman.Year - dogumtarihi.Year;
+            Console.WriteLine(yas + " yaşındasınız");
+
+
+
+            Console.Read();
+        }
+    }
+}
