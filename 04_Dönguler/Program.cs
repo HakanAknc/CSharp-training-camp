0c559024d50a067cac33931ea3d446185a6ac3f3
Hakan Akıncı <96823170+HakanAknc@users.noreply.github.com>
2024-12-10 09:17:31 +0300

    Add files via upload

---
+++ 04_Dönguler/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Dönguler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region While Döngüsü
+
+            //int i = 1;
+
+            //while (i <= 10)
+            //{
+            //    Console.WriteLine("Merhaba Döngüler");
+            //    i++;
+            //}
+
+            //int i = 1;
+
+            //while (i <= 10)
+            //{
+            //    if (i % 3 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //    i++;
+            //}
+
+            //int i = 1;
+            //int sum = 0;
+
+            //while (i <= 10)
+            //{
+            //    sum += i;
+            //    i++;
+            //    //Console.WriteLine(sum);
+            //    //Console.WriteLine(i);
+            //}
+            //Console.WriteLine(sum);
+            //Console.WriteLine(i);
+
+            // Girilen üç sayının toplamı
+            //int a, b, c;
+
+            //Console.Write("a sayısını giriniz: ");
+            //a = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("b sayısını giriniz: ");
+            //b = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("c sayısını giriniz: ");
+            //c = Convert.ToInt32(Console.ReadLine());
+
+            //int toplam = a + b + c;
+            //Console.WriteLine("Toplam : " + toplam);
+
+
+
+            // Örnek soru;
+            // Klavyeden girilen 3 basamaklı sayının basamakları toplamını hesaplayan kodu yazınız.
+
+            Console.Write("Üç basamaklı bir sayı giriniz: ");
+            int number = int.Parse(Console.ReadLine());
+            int ones, tens, hundreds;
+            int sum;
+
+            ones = number % 10;
+            tens = (number % 100) / 10;
+            hundreds = number / 100;
+
+            Console.WriteLine(hundreds + "-" + tens + "-" + ones);
+
+            sum = ones + tens + hundreds;
+            Console.WriteLine(sum);
+
+            
+
+
+
+            Console.Read();
+
+            #endregion
+        }
+    }
+}
---
+++ 04_Loops/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Döngüler
+            //int i;
+
+            //for (i = 1; i <= 5; i++)
+            //{
+            //    Console.WriteLine("C# Eğitim Kampı");
+            //}
+
+            //for (int i = 1; i <= 20; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //for (int i = 3; i <= 50;  i += 3)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //Console.Write("Lütfen ekrana yazılmasını istediğiniz adedi giriniz: ");
+            //int finishValue = int.Parse(Console.ReadLine());
+
+            //for (int i = 0; i < finishValue; i++)
+            //{
+            //    Console.WriteLine("Yaşasın Türkiye");
+            //}
+
+            //Console.Read();
+
+            #endregion
+
+            #region For Döngüsü ile Karar Yapıları
+
+            //for (int i = 1; i <= 100; i++)
+            //{
+            //    if (i % 5 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //int totalvalue = 0;
+
+            //for (int i= 1; i <= 10; i++)
+            //{
+            //    totalvalue += i;
+            //}
+
+            //Console.WriteLine(totalvalue);
+
+            //int totalValue = 0;
+
+            //for (int i = 1; i < 20; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        totalValue += i;
+            //        Console.WriteLine(i);
+            //    }
+            //}
+            //Console.WriteLine("-----------");
+            //Console.WriteLine(totalValue);
+
+            //int count = 0;
+            //for (int i = 1; i <= 50;  i++)
+            //{
+            //    if (i % 7 == 0)
+            //    {
+            //        count++;
+            //    }
+            //}
+            //Console.WriteLine(count);
+
+            // Bakteri Örneği 1-2-4-8-16...
+
+            //int bacterium = 1;
+
+            //for (int i = 0; i <= 24; i++)
+            //{
+            //    bacterium *= 2;
+            //    Console.WriteLine(i + ".Saat Sonunda: " + bacterium);
+            //}
+
+
+            Console.Read();
+
+            #endregion
+
+            #region While Döngüsü
+
+            //int i = 1;
+
+            //while (i <= 10)
+            //{
+            //    Console.WriteLine("Merhaba Döngüler");
+            //    i++;
+            //}
+
+            //int i = 1;
+
+            //while (i <= 10)
+            //{
+            //    i++;
+            //    Console.WriteLine("Merhaba Döngüler");
+            //}
+
+            //Console.Read();
+
+            #endregion
+        }
+    }
+}
