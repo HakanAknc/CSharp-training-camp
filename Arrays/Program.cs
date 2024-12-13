2f6249df7a8dd62f154bf1651c7554f309ed3afd
Hakan Akıncı <96823170+HakanAknc@users.noreply.github.com>
2024-12-13 10:06:57 +0300

    Add files via upload

---
+++ Arrays/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Diziler, her değer için ayrı değişkenler tanımlamak yerine, tek bir değişkende birden fazla değeri depolamak için kullanılır.
+            Bir diziyi bildirmek için değişken türünü köşeli parantezlerle tanımlayın 
+            */
+
+            //string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+            //int[] myNum = { 10, 20, 30, 40 };
+
+            //cars[0] = "Hakan";
+
+            //Console.WriteLine(cars[0]);  // volvo
+
+            // Not: Dizi dizinleri 0 ile başlar: [0] ilk öğedir. [1] ikinci öğedir, vb.
+
+            /*
+             Dizi Uzunluğu
+             Bir dizinin kaç elemandan oluştuğunu bulmak için şu Lengthözelliği kullanın:
+             */
+            //string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+            //Console.WriteLine(cars.Length);
+
+            /*
+             Dizi Oluşturmanın Diğer Yolları
+            C# ile aşinaysanız, newanahtar sözcükle oluşturulan dizileri görmüş olabilirsiniz ve belki de belirli bir boyuta sahip dizileri de görmüşsünüzdür. 
+            C#'ta, bir dizi oluşturmanın farklı yolları vardır:
+             */
+
+            // Create an array of four elements, and add values later
+            string[] cars = new string[4];
+
+            // Create an array of four elements and add values right away 
+            string[] cars1 = new string[4] { "Volvo", "BMW", "Ford", "Mazda" };
+
+            // Create an array of four elements without specifying the size 
+            string[] cars2 = new string[] { "Volvo", "BMW", "Ford", "Mazda" };
+
+            // Create an array of four elements, omitting the new keyword, and without specifying the size
+            string[] cars3 = { "Volvo", "BMW", "Ford", "Mazda" };
+
+            // Not : Ancak, bir diziyi bildirip daha sonra başlatırsanız, şu anahtar sözcüğü kullanmanız gerektiğini unutmamalısınız new:
+
+
+            for (int i = 0; i < cars3.Length; i++)
+            {
+                Console.WriteLine(cars3[i]);
+            }
+
+            Console.WriteLine("**************");
+
+            foreach (string car in cars2)
+            {
+                Console.WriteLine(car);
+            }
+
+            Console.Read();
+
+
+
+        }
+    }
+}
---
+++ OrnekProje7/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrnekProje7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //short x = 5;
+            //long y = 5;
+            //double h = 45.75D;
+            ////Console.WriteLine(h);
+
+            //double myDouble = 45.75;
+            //int myInt = (int)myDouble;       // Automatic casting: int to double
+
+            //Console.WriteLine(myInt);      // Outputs 9
+            //Console.WriteLine(myDouble);   // Outputs 9
+
+            //Console.WriteLine("What is your name?");
+            //string name = Console.ReadLine();
+            //Console.WriteLine(name);
+
+            //string name1 = "hakan Akıncı";
+            //Console.WriteLine(name1.Length);
+            //Console.WriteLine(name1.ToUpper());
+            //Console.WriteLine(name1.ToLower());
+
+
+            //string firstName = "John ";
+            //string lastName = "Doe";
+            //string toplam = ($"{firstName} {lastName}");
+            //string name = string.Concat(firstName, lastName);
+            //Console.WriteLine(toplam);
+
+            //int i = 15;
+            //while (i > 10)
+            //{
+            //    Console.WriteLine("Yuşa Medresesi");
+            //    i--;
+            //}
+
+            /*
+             Do/While Döngüsü
+             Döngü do/while, döngünün bir çeşididir while. 
+             Bu döngü, koşulun doğru olup olmadığını kontrol etmeden önce kod bloğunu bir kez yürütür, ardından koşul doğru olduğu sürece döngüyü tekrarlar.
+             Döngü, koşul yanlış olsa bile her zaman en az bir kez yürütülecektir çünkü kod bloğu koşul test edilmeden önce yürütülür:
+             */
+            //int i = 10;
+            //do
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+            //while (i < 5);
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine("Yuşa medresesi");
+            //}Console.WriteLine("Bitti");
+
+            //for (int i = 0; i < 20; i++) 
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine("Çift : " + i);
+            //    } 
+            //}
+
+            //for (int i = 1; i <= 2; ++i)
+            //{
+            //    Console.WriteLine("Outer: " + i);  // Executes 2 times
+
+            //    // Inner loop
+            //    for (int j = 1; j <= 3; j++)
+            //    {
+            //        Console.WriteLine(" Inner: " + j); // Executes 6 times (2 * 3)
+            //    }
+            //}
+
+            //string[] cars = { "Volvo", "BMW", "Ford", "Audi" };
+            //foreach (string car in cars)
+            //{
+            //    Console.WriteLine(cars[1]);
+            //}
+
+            Console.Read();
+        }
+    }
+}
