15fe7358a75e467946291b7c1e967a1c1036503e
Hakan Akıncı <96823170+HakanAknc@users.noreply.github.com>
2024-12-16 16:55:29 +0300

    Add files via upload

---
+++ OrnekProje8/Araba.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrnekProje8
+{
+    internal class Araba
+    {
+        public int kapiSayisi;
+        public string arabaModel;
+        public string arabaRengi;
+
+
+        // CTOR kısa yol CONSTROCTOR 
+        public Araba()
+        {
+            Console.WriteLine("Araba sınıfının yapıcı metodu çalıştı...");
+        }
+
+        public void motorCalistir()
+        {
+            Console.WriteLine("Motor çalışıyor...");
+        }
+
+        public void kapilarKilitli()
+        {
+            Console.WriteLine("Kapılar kilitleniyor...");
+        }
+    }
+}
---
+++ OrnekProje8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrnekProje8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Araba araba1 = new Araba();   // nesne (obje) oluşturuldu
+
+            araba1.kapiSayisi = 4;
+            araba1.arabaRengi = "Siyah";
+            araba1.arabaModel = "TOGG";
+
+            araba1.motorCalistir();
+            araba1.kapilarKilitli();
+
+            Console.WriteLine("Arabanın kapı sayısı : " + araba1.kapiSayisi);
+            Console.WriteLine("Araba rengi : " + araba1.arabaRengi);
+            Console.WriteLine("Arabanın Modeli : " + araba1.arabaModel);
+
+            Console.Read();
+        }
+    }
+}
